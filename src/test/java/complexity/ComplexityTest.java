12baa44662212f085532e653a774ebedb5fede35
Erik Hooijmeijer <erik.hooijmeijer@42.nl>
2018-09-01 14:22:30 +0200

    Adding tests

---
+++ src/test/java/complexity/ComplexityTest.java
@@ -0,0 +1,30 @@
+package complexity;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class ComplexityTest {
+
+	@Test
+	public void shouldTest() {
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+		assertTrue(true);
+	}
+
+	@Test
+	public void shouldTestFalse() {
+		assertFalse(true);
+		assertFalse(true);
+		assertFalse(true);
+		assertFalse(true);
+		assertFalse(true);
+		assertFalse(true);
+	}
+
+}
